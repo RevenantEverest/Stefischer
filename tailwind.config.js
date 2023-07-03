0b80cefcb17ab1b4290c8f6a31296282884869f6
Stefan Fischer <stefischer@mail.com>
2023-07-03 10:22:47 -0400

    Add flowbite and tailwind pnp workaround

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+const path = require("path");
+const flowbiteThemePatch = path.dirname(require.resolve("flowbite-react"));
+
 module.exports = {
     content: [
+        `${flowbiteThemePatch}/**/*.{js,jsx,ts,tsx}`,
         "./src/**/*.{js,jsx,ts,tsx}"
     ],
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [require('flowbite/plugin')],
 };
 
