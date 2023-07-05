ccab11563eb62aa5080aab1036602f70b4427293
Stefan Fischer <stefischer@mail.com>
2023-07-05 15:07:08 -0400

    Add redux to tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './store';
 
 test('renders learn react link', () => {
-  render(<App />);
+    render(
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <App />
+            </PersistGate>
+        </Provider>
+    );
 });
