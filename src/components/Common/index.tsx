e515d05ab212052fcf47a86ce3210beca7ea431b
Stefan Fischer <stefischer@mail.com>
2024-02-04 21:13:10 -0500

    Create WaveDivider common component

--- src/components/Common/index.tsx
+++ src/components/Common/index.tsx
@@ -1,8 +1,14 @@
+/* Layout */
+export { default as Layout } from './Layout';
+
 /* Buttons */
 export { default as Button } from './Button';
 
 /* Cards */
 export { default as Card } from './Card';
+
+/* Divider */
+export { default as WaveDivider } from './Dividers/WaveDivider';
 
 /* Inputs */
 export { default as TextInput } from './Inputs/TextInput';
