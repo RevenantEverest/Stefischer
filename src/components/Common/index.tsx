e01dee98b95dcb42393dfb9c1ee9b506587587b9
Stefan Fischer <stefischer@mail.com>
2024-02-07 15:15:21 -0500

    Add TiltDivider and SocialIcon to common index

--- src/components/Common/index.tsx
+++ src/components/Common/index.tsx
@@ -9,9 +9,13 @@ export { default as Card } from './Card';
 
 /* Divider */
 export { default as WaveDivider } from './Dividers/WaveDivider';
+export { default as TiltDivider } from './Dividers/TiltDivider';
 
 /* Inputs */
 export { default as TextInput } from './Inputs/TextInput';
 
 /* Alerts */
 export { default as SuccessAlert } from './Alerts/SuccessAlert';
+
+/* Social Icon */
+export { default as SocialIcon } from './SocialIcon';
