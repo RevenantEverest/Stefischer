f20199360355274d4308b897013a3412f0d1dd7a
Stefan Fischer <stefischer@mail.com>
2024-02-07 15:18:04 -0500

    Add homepage components to homepage page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,20 +1,59 @@
-import Lottie from 'lottie-react';
-import programmingAnimation from '../lottie/programming-computer.json';
+import React from 'react';
 
-import type { Theme } from '@@types/theme';
+import { Flex } from 'reflexbox';
+import { GiAstronautHelmet } from "react-icons/gi";
 
-export interface HomePageProps {
-    theme: Theme
-};
+import { Layout, TiltDivider, WaveDivider } from '@@components/Common';
+import Jumbotron from '@@components/HomePage/Jumbotron';
+import PortfolioSummary from '@@components/HomePage/PortfolioSummary';
+import AboutMeSummary from '@@components/HomePage/AboutMeSummary';
+import SkillsAndExperience from '@@components/HomePage/SkillsAndExperience';
 
-function HomePage(props: HomePageProps) {
+function HomePage() {
     return(
-        <div>
-            <h1>Home Page</h1>
-            <div className="h-1/4 w-1/4">
-                <Lottie animationData={programmingAnimation} />
-            </div>
-        </div>
+        <React.Fragment>
+            <Flex className="fixed">
+                <Jumbotron />
+            </Flex>
+            <Flex className="absolute left-0 right-0 top-[66.2vh] md:top-[66.2vh] lg:top-[66.2vh] justify-center bg-red-500">
+                <WaveDivider />
+            </Flex>    
+            <Flex className="relative bg-background w-full justify-center top-[86vh] pt-20">
+                <Layout>
+                    <div className="mb-64">
+                        <AboutMeSummary />
+                    </div>
+                    <div className="mb-28">
+                        <SkillsAndExperience />
+                    </div>
+                </Layout>
+            </Flex>
+            <Flex className="relative bg-background w-full justify-center top-[86vh] pt-20 z-10">
+                <Flex className="absolute w-full justify-center top-[24vh]">
+                    <TiltDivider />
+                </Flex>
+            </Flex>
+            <Flex className="relative bg-gradient-to-tr from-primary to-secondary w-full justify-center top-[86vh] pt-32 pb-64">
+                <Layout>
+                    <div className="">
+                        <PortfolioSummary />
+                    </div>
+                </Layout>
+            </Flex>
+            <Flex className="relative w-full justify-center top-[60vh] pt-20 z-10">
+                <Flex className="absolute w-full justify-center top-[24vh]">
+                    <TiltDivider rotate />
+                </Flex>
+            </Flex>
+            <Flex className="relative bg-background w-full justify-center top-[75vh] py-20">
+                <Layout>
+                    <div className="">
+                        <GiAstronautHelmet className="text-8xl" />
+                    </div>
+                </Layout>
+            </Flex>
+            
+        </React.Fragment>
     );
 };
 
