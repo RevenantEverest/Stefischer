52d6cd3caf9737beaac7084d22f5bc3bf1afa367
Stefan Fischer <stefischer@mail.com>
2024-02-07 15:11:48 -0500

    Add projects and personal links to constants index

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,9 @@
 export * as IMAGE_RESOURCES from './imageResources';
+export * as PERSONAL_LINKS from './personalLinks';
+
+/* Projects */
+export * as LOGO_PROJECTS from './projects/logos/__index__';
+export * as MOTION_GRAPHICS_PROJECTS from './projects/vfx/__index__';
+export * as FRONTEND_PROJECTS from './projects/development/frontend/__index__';
+export * as BACKEND_PROJECTS from './projects/development/backend/__index__';
+export * as FULL_STACK_PROJECTS from './projects/development/fullstack/__index__';
