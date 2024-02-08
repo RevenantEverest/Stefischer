82cfe2b36747f029e5ccc73d101a49c0f4a52353
Stefan Fischer <stefischer@mail.com>
2024-02-08 14:00:05 -0500

    Use BeamMeUp and ContactSection components in Home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,29 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Flex } from 'reflexbox';
-import { GiAstronautHelmet } from "react-icons/gi";
+import { Element, scrollSpy } from 'react-scroll';
 
-import { Layout, TiltDivider, WaveDivider } from '@@components/Common';
-import Jumbotron from '@@components/HomePage/Jumbotron';
-import PortfolioSummary from '@@components/HomePage/PortfolioSummary';
-import AboutMeSummary from '@@components/HomePage/AboutMeSummary';
-import SkillsAndExperience from '@@components/HomePage/SkillsAndExperience';
+import { Layout, TiltDivider, WaveDivider, BeamMeUp } from '@@components/Common';
+
+/* Home */
+import Jumbotron from '@@components/Home/Jumbotron';
+import SkillsAndExperience from '@@components/Home/SkillsAndExperience';
+import TitleBlurbs from '@@components/Home/TitleBlurbs';
+
+/* About */
+import AboutMeSummary from '@@components/About/AboutMeSummary';
+
+/* Portfolio */
+import PortfolioSummary from '@@components/Portfolio/PortfolioSummary';
+
+/* Contact */
+import ContactSection from '@@components/Contact/ContactSection';
+
+import { SCROLL_ELEMENTS } from '@@constants';
 
 function HomePage() {
+
+    useEffect(() => {
+        scrollSpy.update();
+    }, []);
+
     return(
         <React.Fragment>
             <Flex className="fixed">
-                <Jumbotron />
+                <Jumbotron scrollElements={SCROLL_ELEMENTS.HOMEPAGE} />
             </Flex>
             <Flex className="absolute left-0 right-0 top-[66.2vh] md:top-[66.2vh] lg:top-[66.2vh] justify-center bg-red-500">
                 <WaveDivider />
             </Flex>    
             <Flex className="relative bg-background w-full justify-center top-[86vh] pt-20">
                 <Layout>
-                    <div className="mb-64">
-                        <AboutMeSummary />
+                    <Element name={SCROLL_ELEMENTS.HOMEPAGE.about}>
+                        <div className="my-32">
+                            <AboutMeSummary />
+                        </div>
+                    </Element>
+                    <div className="my-32">
+                        <TitleBlurbs />
                     </div>
-                    <div className="mb-28">
+                    <div className="mb-32">
                         <SkillsAndExperience />
                     </div>
                 </Layout>
@@ -47,8 +69,13 @@ function HomePage() {
             </Flex>
             <Flex className="relative bg-background w-full justify-center top-[75vh] py-20">
                 <Layout>
-                    <div className="">
-                        <GiAstronautHelmet className="text-8xl" />
+                    <Element name={SCROLL_ELEMENTS.HOMEPAGE.contact}>
+                        <div className="my-32">
+                            <ContactSection />
+                        </div>
+                    </Element>
+                    <div className="flex items-center justify-center">
+                        <BeamMeUp />
                     </div>
                 </Layout>
             </Flex>
