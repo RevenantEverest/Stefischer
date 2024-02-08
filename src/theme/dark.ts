b53147b6a13f7c381c0e0d8f20132bb2eb4c79ef
Stefan Fischer <stefischer@mail.com>
2024-02-08 13:56:21 -0500

    Add card light property in dark theme

--- src/theme/dark.ts
+++ src/theme/dark.ts
@@ -1,14 +1,12 @@
 import type { Theme, ThemeColors } from '@@types/theme';
 
 const colors: ThemeColors = {
-    // primary: "#4D11D7",
     primary: "#4D11D7",
-    // secondary: "#F26E3F",
     secondary: "#D25022",
     accent: "",
     background: "#1A1A1A",
     card: "#2A2A2A",
-    cardLight: "",
+    cardLight: "#3A3A3A",
     text: "#DEDEDE",
     muted: "#7C7C7C",
     accentText: "#F26E3F"
