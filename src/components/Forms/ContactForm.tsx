e9803adcb49cdb664be5fa527974b0822aa4e7dc
Stefan Fischer <stefischer@mail.com>
2024-02-08 13:59:12 -0500

    Create ContactForm component

---
+++ src/components/Forms/ContactForm.tsx
@@ -0,0 +1,110 @@
+import { useFormik } from 'formik';
+import { Flex } from 'reflexbox';
+import { faEnvelope, faPhone, faUser, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+
+import type { FormikHelpers } from 'formik';
+
+import { TextInput, TextArea, Button } from '@@components/Common';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export interface ContactFormValues {
+    name: string,
+    email: string,
+    phoneNumber: string,
+    message: string
+};
+
+export type ContactFormHelpers = FormikHelpers<ContactFormValues>;
+
+export interface ContactFormProps {
+    onSubmit: (values: ContactFormValues, helpers: ContactFormHelpers) => void
+};
+
+function ContactForm({ onSubmit }: ContactFormProps) {
+    
+    const {
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        values,
+        errors
+    } = useFormik({
+        initialValues: {
+            name: "",
+            email: "",
+            phoneNumber: "",
+            message: ""
+        },
+        onSubmit
+    });
+
+    return(
+        <Flex>
+            <form className="w-full" onSubmit={handleSubmit}>
+                <div className="my-3">
+                    <TextInput
+                        id="name"
+                        type="text"
+                        icon={faUser}
+                        placeholder="Your Name"
+                        label="Name"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.name}
+                        errorMessage={errors.name}
+                        required={true}
+                    />
+                </div>
+                <div className="my-3">
+                    <TextInput
+                        id="email"
+                        type="email"
+                        icon={faEnvelope}
+                        placeholder="example@example.com"
+                        label="Email"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.email}
+                        errorMessage={errors.email}
+                        required={true}
+                    />
+                </div>
+                <div className="my-3">
+                    <TextInput
+                        id="phoneNumber"
+                        type="text"
+                        icon={faPhone}
+                        placeholder="1 (555)-555-5555"
+                        label="Phone Number"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.phoneNumber}
+                        errorMessage={errors.phoneNumber}
+                        required={true}
+                    />
+                </div>
+                <div className="my-3">
+                    <TextArea
+                        id="message"
+                        placeholder="Message here"
+                        label="Message"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.phoneNumber}
+                        errorMessage={errors.phoneNumber}
+                        required={true}
+                    />
+                </div>
+                <div className="mt-10 mb-5">
+                    <Button className="w-full" color="secondary" disabled={isSubmitting} onClick={() => handleSubmit}>
+                        <p className="font-semibold">Submit</p>
+                        <FontAwesomeIcon className="ml-2" icon={faPaperPlane} />
+                    </Button>
+                </div>
+            </form>
+        </Flex>
+    );
+};
+
+export default ContactForm;
