e16e82a10ee15caae1548f89a81eb11e0b47f39c
Stefan Fischer <stefischer@mail.com>
2024-02-15 22:57:45 -0500

    Add constants for portfolio project categories

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -8,3 +8,4 @@ export * as MOTION_GRAPHICS_PROJECTS from './projects/vfx/__index__';
 export * as FRONTEND_PROJECTS from './projects/development/frontend/__index__';
 export * as BACKEND_PROJECTS from './projects/development/backend/__index__';
 export * as FULL_STACK_PROJECTS from './projects/development/fullstack/__index__';
+export * as PROJECT_CATEGORIES from './projects/categories';
