d1c2a758f5403f3a0d630039cce769de0fd5d85e
Stefan Fischer <stefischer@mail.com>
2024-02-19 09:19:34 -0500

    Add path alias to jest config and jest canvas mock

--- jest.config.ts
+++ jest.config.ts
@@ -3,10 +3,30 @@ import type { Config } from 'jest';
 const jestConfig: Config = {
     preset: "ts-jest",
     testEnvironment: "jest-environment-jsdom",
-    rootDir: "src",
+    testMatch: [
+        "**/__tests__/**/*.test.tsx",
+        "**/__tests__/**/*.spec.tsx"
+    ],
     transform: {
         "^.+\\.tsx?$": "ts-jest"
-    }
+    },
+    moduleNameMapper: {
+        "^@@root/(.*)$": "<rootDir>/src/$1",
+        "^@@assets/(.*)$": "<rootDir>/src/assets/$1",
+        "^@@components/(.*)$": "<rootDir>/src/components/$1",
+        "^@@containers/(.*)$": "<rootDir>/src/containers/$1",
+        "^@@navigation/(.*)$": "<rootDir>/src/navigation/$1",
+        "^@@pages/(.*)$": "<rootDir>/src/pages/$1",
+        "^@@store/(.*)$": "<rootDir>/src/store/$1",
+        "^@@types/(.*)$": "<rootDir>/src/types/$1",
+        "^@@theme/(.*)$": "<rootDir>/src/theme/$1",
+        "^@@tests/support/(.*)$": "<rootDir>/src/__tests__/support/$1",
+
+        "^@@constants": "<rootDir>/src/constants/index.ts",
+        "^@@hooks": "<rootDir>/src/hooks/index.tsx",
+        "^@@utils": "<rootDir>/src/utils/index.ts",
+    },
+    setupFiles: ["jest-canvas-mock"]
 };
 
 export default jestConfig;
