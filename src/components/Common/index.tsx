d5278a3a26fb7d026679ab400010a72f52ce66f4
Stefan Fischer <stefischer@mail.com>
2024-02-21 03:28:22 -0500

    Update common component index

--- src/components/Common/index.tsx
+++ src/components/Common/index.tsx
@@ -1,15 +1,3 @@
-/* Layout */
-export { default as Layout } from './Layout';
-
-/* Buttons */
-export { default as Button } from './Button';
-
-/* Cards */
-export { default as Card } from './Card';
-
-/* Modal */
-export { default as Modal } from './Modal';
-
 /* Divider */
 export { default as WaveDivider } from './Dividers/WaveDivider';
 export { default as TiltDivider } from './Dividers/TiltDivider';
@@ -18,12 +6,15 @@ export { default as TiltDivider } from './Dividers/TiltDivider';
 export { default as TextInput } from './Inputs/TextInput';
 export { default as TextArea } from './Inputs/TextArea';
 
-/* Alerts */
-export { default as SuccessAlert } from './Alerts/SuccessAlert';
-
-/* Social Icon */
-export { default as SocialIcon } from './SocialIcon';
+/* Toasts */
+export { default as ToastError } from './Toasts/ToastError';
+export { default as ToastSuccess } from './Toasts/ToastSuccess';
 
 /* Other */
 export { default as BeamMeUp } from './BeamMeUp';
+export { default as Button } from './Button';
+export { default as Card } from './Card';
+export { default as Layout } from './Layout';
+export { default as Modal } from './Modal';
+export { default as SocialIcon } from './SocialIcon';
 export { default as YouTubeEmbed } from './YouTubeEmbed';
