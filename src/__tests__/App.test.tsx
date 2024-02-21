da14f61bac4c6d1c522967f332e4e5244406730c
Stefan Fischer <stefischer@mail.com>
2024-02-20 19:06:31 -0500

    Update App test to render component

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,3 +1,11 @@
-test("demo", () => {
+import '@testing-library/jest-dom';
+import './mocks/intersectionObserver';
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { HomePage } from '@@pages/index';
+
+/* Example Page Test */
+test("Home Page", () => {
+    render(<HomePage />, { wrapper: BrowserRouter })
     expect(true).toBe(true);
 });
