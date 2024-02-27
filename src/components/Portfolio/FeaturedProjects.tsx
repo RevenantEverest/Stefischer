31405e63f38871bae09c2bb30bc91f0fb8b3a55f
Stefan Fischer <stefischer@mail.com>
2024-02-27 10:15:31 -0500

    Adjust mobile styling in various components

--- src/components/Portfolio/FeaturedProjects.tsx
+++ src/components/Portfolio/FeaturedProjects.tsx
@@ -35,7 +35,7 @@ function FeaturedProjects() {
         <Flex className="flex-col w-full items-center">
             <Box className="flex flex-col justify-center items-center pb-20 w-full">
                 <h1 className="text-6xl font-semibold text-center">Featured Projects</h1>
-                <p className="w-3/6 mt-6 font-semibold text-center">
+                <p className="w-full md:w-3/6 mt-6 font-semibold text-center">
                     Explore my featured projects, merging full-stack development, graphic design, and visual effects into 
                     a dynamic portfolio that reflects my diverse skill set.
                 </p>
