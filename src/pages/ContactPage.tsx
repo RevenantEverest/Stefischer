1abfee9d743f38c125f8d2a8c0309a9e741c7507
Stefan Fischer <stefischer@mail.com>
2024-02-27 10:15:01 -0500

    Update mobile responsiveness in ContactPage header

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -12,13 +12,13 @@ function ContactPage() {
                 <title>Stefan Fischer - Contact</title>
             </Helmet>
             <Flex className="fixed w-full">
-                <Box className="flex w-full h-5/6 py-20 px-4 md:px-40 bg-gradient-to-tr from-primary to-secondary justify-center items-center">
+                <Box className="flex w-full h-11/12 py-10 lg:py-20 px-4 md:px-40 bg-gradient-to-tr from-primary to-secondary justify-center items-center">
                     <Flex className="h-[70vh] md:h-[66vh] lg:h-[40vh] pt-32">
-                        <h1 className="text-8xl font-bold uppercase text-white/20">Contact</h1>
+                        <h1 className="text-6xl md:text-7xl lg:text-8xl font-bold uppercase text-white/20">Contact</h1>
                     </Flex>
                 </Box>
             </Flex>
-            <Flex className="absolute left-0 right-0 top-[66.2vh] md:top-[66.2vh] lg:top-[36.2vh] justify-center bg-red-500">
+            <Flex className="absolute left-0 right-0 top-[36.2vh] md:top-[60.2vh] lg:top-[36.2vh] justify-center">
                 <WaveDivider />
             </Flex>
             <Flex className="relative bg-background w-full justify-center top-[56vh] pt-20">
