90dcfb39f5ec17b0b891f3dbf47b36d4ff29f6fc
Stefan Fischer <stefischer@mail.com>
2024-02-28 09:57:50 -0500

    Add archiver and workTimeManager projects to project constants

--- src/constants/projects/development/backend/__index__.ts
+++ src/constants/projects/development/backend/__index__.ts
@@ -1,10 +1,15 @@
 import type { DeveloperProject } from '@@types/Portfolio';
 
+import Archiver from './archiver';
 import GW2EventCrawler from './gw2EventCrawler';
 
+/*
+    Order projects here for display on Portfolio
+*/
 const BackendProjects: DeveloperProject[] = [
+    Archiver,
     GW2EventCrawler
 ];
 
-export { GW2EventCrawler };
+export { Archiver, GW2EventCrawler };
 export default BackendProjects;
---
+++ src/constants/projects/development/fullstack/workTimeManager.ts
@@ -0,0 +1,25 @@
+import type { DeveloperProject } from '@@types/Portfolio';
+
+const WorkTimeManager: DeveloperProject = {
+    title: "Work TIme Manager",
+    subtitle: "A web client that tracks time spent on projects",
+    description: `
+        A web client that tracks time spent on projects. Created to help create invoices while freelancing.
+    `,
+    thumbnailUrl: "",
+    github: "https://github.com/RevenantEverest/WorkTimeManager",
+    deployed: false,
+    images: [
+    
+    ],
+    technologies: [
+        "Node.js",
+        "Express",
+        "PostgreSQL",
+        "React.js",
+        "Redux"
+    ],
+    category: "Full-Stack"
+};
+
+export default WorkTimeManager;
