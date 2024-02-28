29ac40c9ed9c7efef02d31b63d4ccf14c8c5d3fd
Stefan Fischer <stefischer@mail.com>
2024-02-28 11:00:21 -0500

    Add non placeholder descriptions to timeline points in DeveloperTimeline

--- src/components/About/DeveloperTimeline.tsx
+++ src/components/About/DeveloperTimeline.tsx
@@ -17,36 +17,54 @@ function DeveloperTimeline() {
         {
             date: "March 2018",
             title: "Started FiresideBOT",
-            description: "Began Web Development Immersive (WDI) course at General Assembly. Learned JavaScript, JQuery, Express, React, and PostgreSQL",
+            description: `
+                Coming out of the WDI course, I wanted to start building out my portfolio. My first project was 
+                FiresideBOT, a Discord bot. This project helped hone the skills I learned during the WDI course.
+            `,
             extraContent: () => <ViewProjectButton />
         },
         {
             date: "May 2018",
             title: "Started Cognizant Sponsored Course",
-            description: "Began Web Development Immersive (WDI) course at General Assembly. Learned JavaScript, JQuery, Express, React, and PostgreSQL",
+            description: `
+                Began Software Engineer in Training (SET) course at General Assembly. This course was sponsored by 
+                Fortune 500 company Cognizant. Here I learned about Java, Spring Boot, Docker and microservice architecture.
+            `,
             extraContent: () => <ViewProjectButton />
         },
         {
             date: "June 2018",
             title: "Created Acirhia",
-            description: "Began Web Development Immersive (WDI) course at General Assembly. Learned JavaScript, JQuery, Express, React, and PostgreSQL",
+            description: `
+                A 2D browser based RPG, where you can fight, accept/complete quests, and buy/sell at shops. This 
+                was made as a final project for the SET course.
+            `,
             extraContent: () => <ViewProjectButton />
         },
         {
             date: "November 2018",
             title: "Launched FiresideBOT",
-            description: "Began Web Development Immersive (WDI) course at General Assembly. Learned JavaScript, JQuery, Express, React, and PostgreSQL"
+            description: `
+                Initially created to deepen my knowledge about full stack interactions, I decided to launch FiresideBOT to the public.
+                During the first few weeks garnering over 40,000 users.
+            `
         },
         {
             date: "March 2020",
             title: "Started DungeonTracker",
-            description: "",
+            description: `
+                A full stack project with the aim of making D&D campaigns easier to track for both players and DM.
+            `,
             extraContent: () => <ViewProjectButton />
         },
         {
             date: "April 2020",
             title: "Created Archiver",
-            description: "A Discord bot that migrates entire text channel history to another text channel (even in another Discord server).",
+            description: `
+                A Discord bot that migrates full historical text channel messages and pins to another text channel (even to another Discord server).
+                This was created when my friend groups Discord server was being moved to another server, but we wanted to keep all the channels pins 
+                and messages available to view. 
+            `,
             extraContent: () => <ViewProjectButton />
         },
         {
@@ -58,23 +76,33 @@ function DeveloperTimeline() {
         {
             date: "May 2021",
             title: "Started WorkTimeManager",
-            description: "A web client that tracks time spent on projects. Created to help create invoices while freelancing.",
+            description: `
+                A web client that tracks time spent on projects. Created to help create invoices while freelancing.
+            `,
             extraContent: () => <ViewProjectButton />
         },
         {
             date: "August 2021",
             title: "Started at Stackend Solutions LLC",
-            description: "Began working at Stackend Solutions LLC as a Full Stack Developer. Here I help other startup founders build their products from the ground up."
+            description: `
+                Began working at Stackend Solutions LLC as a Full Stack Developer. Here I help other startup 
+                founders build their products from the ground up. It's here that I learned Python, Django, and React Native.
+            `
         },
         {
             date: "March 2022",
             title: "Began rewriting FiresideBOT",
-            description: "Due to a change Discord planned on implementing, the codebase for Fireside needed to be rewritten."
+            description: `
+                Due to a change Discord planned on implementing, the codebase for Fireside needed to be rewritten.
+            `
         },
         {
             date: "March 2023",
             title: "Create GW2 Event Tracker",
-            description: "After needing event information for GW2 and not being able to find a good API, I created this web scraper to scrape the information from the GW2 Wiki.",
+            description: `
+                After needing event information for GW2 and not being able to find a good API, I created this web 
+                scraper to scrape the information from the GW2 Wiki.
+            `,
             extraContent: () => <ViewProjectButton />
         },
     ];
@@ -90,7 +118,7 @@ function DeveloperTimeline() {
                     career milestones. Dive into the moments that have influenced my skills and professional path.
                 </p>
             </Box>
-            <Box>
+            <Box className="w-full md:w-5/6">
                 <Timeline pointIcon={HiCalendar} data={timeline} />
             </Box>
         </Flex>
