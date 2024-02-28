28ab06cd9dc4f12331ca208a12fb4fd00d3780cf
Stefan Fischer <stefischer@mail.com>
2024-02-28 14:08:54 -0500

    Add new logo projects

---
+++ src/constants/projects/logos/weekendWarrior.ts
@@ -0,0 +1,10 @@
+import type { Project } from '@@types/Portfolio';
+
+const WeekendWarrior: Project = {
+    title: "Weekend Warrior",
+    subtitle: "Logo done for a local business",
+    thumbnailUrl: "https://i.imgur.com/a65dJFt.png",
+    category: "Logos"
+};
+
+export default WeekendWarrior;
