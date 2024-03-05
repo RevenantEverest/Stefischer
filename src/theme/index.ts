e8a63f78e29594088fbe73637b9cb2cd51862b32
Stefan Fischer <stefischer@mail.com>
2024-03-04 19:33:36 -0500

    Create new themes

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,1 +1,4 @@
 export { default as dark } from './dark';
+export { default as light } from './light';
+export { default as cyberpunk } from './cyberpunk';
+export { default as warm } from './warm';
