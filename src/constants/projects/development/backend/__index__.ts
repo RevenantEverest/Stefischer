89602c273f1d85e1172c5c097413f50b82b16520
Stefan Fischer <stefischer@mail.com>
2024-12-04 16:49:17 -0500

    Add Spindle backend project

--- src/constants/projects/development/backend/__index__.ts
+++ src/constants/projects/development/backend/__index__.ts
@@ -2,14 +2,16 @@ import type { DeveloperProject } from '@@types/Portfolio';
 
 import Archiver from './archiver';
 import GW2EventCrawler from './gw2EventCrawler';
+import Spindle from './spindle';
 
 /*
     Order projects here for display on Portfolio
 */
 const BackendProjects: DeveloperProject[] = [
     Archiver,
+    Spindle,
     GW2EventCrawler
 ];
 
-export { Archiver, GW2EventCrawler };
+export { Archiver, GW2EventCrawler, Spindle };
 export default BackendProjects;
